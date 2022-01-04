7422017f74ec501fd252d079708fed4ea1b15194
Aparna-devi-chinnu-R <aparnadevi.rajavel@thoughtworks.com>
2022-01-04 16:25:51 +0530

    [Aparna/Hruthika] GCP SQL.

--- src/main/java/com/sample/google_cloud_run_demo/HelloWorldController.java
+++ src/main/java/com/sample/google_cloud_run_demo/HelloWorldController.java
@@ -1,16 +1,32 @@
 package com.sample.google_cloud_run_demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin("*") //TODO : Revisit  for proper url
 public class HelloWorldController {
 
-    @GetMapping("/home")
-    public String returnHelloWorld(){
-        return "Hello world";
+    private final HelloWorldRepository helloWorldRepository;
+
+    @Autowired
+    public HelloWorldController(HelloWorldRepository helloWorldRepository) {
+        this.helloWorldRepository = helloWorldRepository;
     }
+
+    @GetMapping("/home/{id}")
+    public String returnHelloWorld(@PathVariable Long id){
+        String name = helloWorldRepository.findId(id).getUserName();
+        return "Hello " + name;
+    }
+
+
+    /*
+       DATABASE_URL  "jdbc:postgresql://35.200.255.247:5432/postgres"
+       DATABASE_USERNAME ayojak_user
+       DATABASE_PASSWORD ayojak_user
+     */
 
 }
